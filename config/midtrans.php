50a0251bdb4852704c11d5d7011e1090b4603f83
marprin <marprin93@gmail.com>
2018-01-02 17:53:20 +0700

    Update Config midtrans to make is_production available in .env

--- config/midtrans.php
+++ config/midtrans.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'is_production' => true,
+    'is_production' => env('MIDTRANS_IS_PRODUCTION', true),
     'client_key' => env('MIDTRANS_CLIENT_KEY'),
     'server_key' => env('MIDTRANS_SERVER_KEY')
 ];
