fb967e225ca0ffa2ea4dc531c2b66b509188d22a
marprin <marprin93@gmail.com>
2018-01-26 16:36:14 +0700

    Prepare unit test

---
+++ tests/MidtransTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Marprinhm\Midtrans;
+
+use PHPUnit_Framework_TestCase;
+
+class MidtransTest extends PHPUnit_Framework_TestCase {
+
+    public function testCases() {
+        
+        $this->assertEquals(true, true);
+    }
+}
